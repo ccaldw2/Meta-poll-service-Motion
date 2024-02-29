aac91b9760d29ce2fe736d5b3729856f52267446
Caleb Caldwell <ccaldw2@uwo.ca>
2024-02-29 13:53:14 -0500

    wrapper class for Meta request completed

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,12 @@
 "use strict"
-console.log("Boilerplate configured")
+import dotenv from 'dotenv'
+import fetch_data from "./meta/fetch_data"
+
+dotenv.config()
+
+const access_token: string|undefined = process.env.META_ACCESS_TOKEN
+
+
+if (typeof access_token === "string") {
+  fetch_data(access_token)
+}
---
+++ src/meta/fetch_data.ts
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+async function fetch_data(access_token:string) {
+  try {
+    let meta_api_version: string = "v19.0"
+    let fetch_fields: string[] = ["id", "first_name", "last_name"]
+    let fetch_fields_concat: string = fetch_fields.join("%2C")
+    
+    let url: string = `https://graph.facebook.com/${meta_api_version}/me?fields=${fetch_fields_concat}&access_token=${access_token}`;
+  
+    let response = await axios.get(url)
+    let data = response.data
+
+    return data;
+  } catch (error) {
+   return error 
+  }
+}
+
+export default fetch_data;
