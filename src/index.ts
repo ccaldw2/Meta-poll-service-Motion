fb4119a2c66d0d7bd66e8c80c852cbf432c0b97d
Caleb Caldwell <ccaldw2@uwo.ca>
2024-02-29 15:26:54 -0500

    bit of cleanup

--- src/index.ts
+++ src/index.ts
@@ -4,25 +4,25 @@ import fetch_data from "./meta/fetch_data";
 import { RateLimiter } from "limiter";
 
 async function main() {
-  dotenv.config()
+  dotenv.config();
 
 
-  const access_token: string|null = (process.argv.slice(2)[0] || null)
+  const access_token: string|null = (process.argv.slice(2)[0] || null);
 
   if (typeof access_token === "string") {
 
-    const limiter = new RateLimiter({ tokensPerInterval: 1, interval: 2000})
+    const limiter: RateLimiter = new RateLimiter({ tokensPerInterval: 1, interval: 2000});
 
     while(true) {
-      const remainingRequests = await limiter.removeTokens(1)
-      let data_fetched = await fetch_data(access_token)
-      console.log(data_fetched)
+      const remainingRequests: number = await limiter.removeTokens(1);
+      let data_fetched: Object = await fetch_data(access_token);
+      console.log(data_fetched);
     }
   } 
   else {
-    console.log("Access token required to poll Facebook API")
+    console.log("Access token required to poll Facebook API");
   }
 }
 
 
-main()
+main();
--- src/meta/fetch_data.ts
+++ src/meta/fetch_data.ts
@@ -1,20 +1,20 @@
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
 
 async function fetch_data(access_token:string) {
   try {
-    let meta_api_version: string|undefined = process.env.META_API_VERSION
-    
-    let fetch_fields: string[] = ["id", "first_name", "last_name"]
-    let fetch_fields_concat: string = fetch_fields.join("%2C")
+    let meta_api_version: string|undefined = process.env.META_API_VERSION;
+
+    let fetch_fields: string[] = ["id", "first_name", "last_name"];
+    let fetch_fields_concat: string = fetch_fields.join("%2C");
     
     let url: string = `https://graph.facebook.com/${meta_api_version}/me?fields=${fetch_fields_concat}&access_token=${access_token}`;
   
-    let response = await axios.get(url)
-    let data = response.data
+    let response: AxiosResponse = await axios.get(url);
+    let data: object = response.data;
 
     return data;
   } catch (error) {
-   return error 
+   return error;
   }
 }
 
