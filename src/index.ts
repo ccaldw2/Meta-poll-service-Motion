09ea6318a6b2507da3584fa03609fa8a8fe3c730
Caleb Caldwell <ccaldw2@uwo.ca>
2024-02-29 15:04:04 -0500

    added 2 second rate limit between requests

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
 "use strict"
 import dotenv from 'dotenv';
 import fetch_data from "./meta/fetch_data";
-import yargs from 'yargs';
-
+import { RateLimiter } from "limiter";
 
 async function main() {
   dotenv.config()
@@ -11,11 +10,18 @@ async function main() {
   const access_token: string|null = (process.argv.slice(2)[0] || null)
 
   if (typeof access_token === "string") {
-    fetch_data(access_token)
-  } else {
+
+    const limiter = new RateLimiter({ tokensPerInterval: 1, interval: 2000})
+
+    while(true) {
+      const remainingRequests = await limiter.removeTokens(1)
+      let data_fetched = await fetch_data(access_token)
+      console.log(data_fetched)
+    }
+  } 
+  else {
     console.log("Access token required to poll Facebook API")
   }
-
 }
 
 
